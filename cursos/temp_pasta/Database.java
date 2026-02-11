de6dd91e0a67ba765dd5dde60cb6e98b351a894b
Matheus Vieira <matheussilvavieira07@gmail.com>
2026-02-11 16:35:27 -0300

    [FIX] Renomeando projeto para temp_pasta e corrigindo sintaxe Java

---
+++ cursos/temp_pasta/Database.java
@@ -0,0 +1,21 @@
+package cursos.projeto_login;
+
+public class Database {
+
+    public void conectar() {
+        int tentativas = 1;
+        boolean conectado = false;
+
+        while (tentativas <= 3 && !conectado) {
+            System.out.println("Tentando conectar ao banco... Tentativa: " + tentativas);
+            
+            if (tentativas < 3) {
+                tentativas++;
+            } else {
+                // REGRA DE SEGURANÇA: Mensagem genérica para o usuário final
+                System.out.println("ERRO: Sistema indisponível no momento. Tente novamente mais tarde.");
+                break; 
+            }
+        }
+    }
+}
